c0bf35638188c7109c7930f54af7555ebd9f97e4
King-fReeBoDy <89941338+King-fReeBoDy@users.noreply.github.com>
2023-09-19 12:04:54 +0000

    cart and product card update

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -6,44 +6,44 @@ import { Link } from "react-router-dom";
 
 const Products = () => {
   return (
-    <Link to="/products">
-      <div className=" shrink-0 w-[300px] md:w-[320px]">
-        <div className="p-5 bg-gray-900 rounded-3xl mb-5">
-          <div className="flex justify-end">
-            <p className="bg-black p-2 rounded-full">
-              <i>
-                <AiOutlineHeart className="text-2xl" />
-              </i>
-            </p>
-          </div>
+    <div className=" shrink-0 w-[300px] md:w-[320px] group">
+      <div className="relative p-5 bg-gray-900 rounded-3xl mb-5 overflow-hidden">
+        <div className="flex justify-end">
+          <p className="bg-black p-2 rounded-full">
+            <i>
+              <AiOutlineHeart className="text-2xl" />
+            </i>
+          </p>
+        </div>
 
+        <Link to="/products">
           <div>
             <img src="/assets/assets/IMG_1966.PNG" alt="" />
           </div>
+        </Link>
+        <section className="translate-y-[200%] group-hover:translate-y-0 transition-all duration-300 absolute bottom-5 left-6 z-10 flex justify-center items-center space-x-2 text-sm ">
+          <div className="flex items-center bg-white px-5 py-2 rounded-full shadow-lg text-black">
+            <i className="mr-2">
+              <PiBag />
+            </i>
+            <p>Add to bag</p>
+          </div>
 
-          <section className="hidden md:flex justify-center items-center space-x-2 text-sm">
-            <div className="flex items-center bg-white px-5 py-2 rounded-full shadow-lg text-black">
-              <i className="mr-2">
-                <PiBag />
-              </i>
-              <p>Add to bag</p>
-            </div>
+          <div className="flex items-center bg-black  px-5 py-2 rounded-full shadow-lg">
+            <i className="mr-2">
+              <HiArrowsPointingOut />
+            </i>
+            <p>Quick view</p>
+          </div>
+        </section>
+      </div>
 
-            <div className="flex items-center bg-black  px-5 py-2 rounded-full shadow-lg">
-              <i className="mr-2">
-                <HiArrowsPointingOut />
-              </i>
-              <p>Quick view</p>
-            </div>
-          </section>
-        </div>
-
-        <div className="mb-3 space-x-3 flex">
-          <p className="w-10 h-5 rounded-full bg-red-500"></p>
-          <p className="w-10 h-5 rounded-full bg-blue-500"></p>
-          <p className="w-10 h-5 rounded-full bg-green-500"></p>
-        </div>
-
+      <div className="mb-3 space-x-3 flex">
+        <p className="w-10 h-5 rounded-full bg-red-500"></p>
+        <p className="w-10 h-5 rounded-full bg-blue-500"></p>
+        <p className="w-10 h-5 rounded-full bg-green-500"></p>
+      </div>
+      <Link to="/products">
         <p className="font-bold text-slate-100 mb-2">Leather Gloves</p>
         <p className="mb-5">Perfect mint green</p>
 
@@ -56,8 +56,8 @@ const Products = () => {
             <p> 4.4(61 reviews)</p>
           </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 };
 
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,5 +1,80 @@
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <section className="w-[90%] mx-auto py-5">
+      <div className=" border-b border-slate-500 mb-5">
+        <h2 className="text-3xl font-bold text-slate-100">Shopping Cart</h2>
+        <p className="text-sm my-5 ">
+          Homepage / Clothing Categories /{" "}
+          <span className="underline">Shopping Cart</span>
+        </p>
+      </div>
+
+      <div className="grid grid-cols-2 gap-3 py-10 border-b border-slate-800 mb-5">
+        <div className="bg-slate-100 rounded-xl">
+          <img src="/public/assets/assets/IMG_1968.PNG" alt="" />
+        </div>
+        <div className="space-y-3">
+          <p className="truncate">Leather Gloves</p>
+          <div className="flex items-center space-x-3">
+            <p>/// Black</p>
+            <p>/// 2xl</p>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <select name="" id="" className="py-2 px-3 bg-gray-800 rounded-lg">
+              <option value="">1</option>
+            </select>
+
+            <p className="border-2 rounded-lg py-1 px-3 border-green-500 text-green-500">
+              $74.00
+            </p>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <p className="px-2 p-1 text-xs rounded-full border whitespace-nowrap">
+              /// In Stock
+            </p>
+            <p className="text-sm text-blue-900">Remove</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="py-10">
+        <h2 className="text-xl text-slate-100 font-bold mb-5">Order Summary</h2>
+
+        <section className="mb-5">
+          <div className="flex justify-between items-center border-b py-5 border-slate-800">
+            <p>Subtotal</p>
+            <p className="text-slate-100 font-bold">$249.000</p>
+          </div>
+          <div className="flex justify-between items-center border-b py-5 border-slate-800">
+            <p>Shipping estimate</p>
+            <p className="text-slate-100 font-bold">$249.000</p>
+          </div>
+          <div className="flex justify-between items-center border-b py-5 border-slate-800">
+            <p>Tax extimate</p>
+            <p className="text-slate-100 font-bold">$249.000</p>
+          </div>
+          <div className="flex justify-between items-center py-5">
+            <p className="text-lg text-slate-100 font-bold">order total</p>
+            <p className="text-slate-100 font-bold">$249.000</p>
+          </div>
+        </section>
+
+        <button className="w-full p-3 bg-slate-100 text-slate-900 rounded-full ">
+          Checkout
+        </button>
+      </div>
+
+      <div className="flex space-x-3">
+        <p>///</p>
+        <p>
+          Learn more <span className="text-slate-100 underline">Taxes</span> and{" "}
+          <span className="text-slate-100 underline">Shipping</span> information
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Cart;
